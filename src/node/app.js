1dd1c25152f8fef065f5754bc8f19ddc118368de
Nikelin96 <n.revutskyi@gmail.com>
2021-09-18 22:29:26 +0300

    Node.js Server
    Completed implementation of node with CRUD operations

--- src/node/app.js
+++ src/node/app.js
@@ -4,10 +4,9 @@ const querystring = require('querystring');
 const express = require('express')
 const { Client } = require('pg');
 const app = express();
+const PostgresRepository = require('./postgresRepository');
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const client = new Client({
@@ -23,112 +22,68 @@ client.connect(function(error) {
     console.log('Connected!');
 });
 
-const GetHeroes = async() => {
-    console.log('Receiving heroes');
-
-    var records = await client.query('SELECT * FROM "Hero"');
-
-    return records.rows.map(hero => {
-        return { id: hero.Id, name: hero.Name }
-    });
-};
-
-const DeleteHero = async(id) => {
-    console.log('Deleting hero by id: ', id);
-
-    const query = 'DELETE FROM "Hero" WHERE "Id" = ' + id;
-    await client.query(query);
-};
-
-const CreateHero = async(hero) => {
-    console.log('Creating hero');
-
-    const query = `INSERT INTO "Hero" ("Name") 
-        VALUES ('` + hero.name + `');`;
-
-    await client.query(query);
-};
-
-const UpdateHero = async(hero) => {
-    console.log('Updating hero');
-
-    const query = `UPDATE "Hero" 
-    SET "Name" ='` + hero.name + `'
-    WHERE "Id" = ` + hero.id;
-
-    await client.query(query);
-};
+const repository = new PostgresRepository(client);
 
 app.get('/', async(request, response) => {
     console.log(`GET heroes`);
 
-    let heroes = await GetHeroes();
+    let heroes = await repository.getHeroesAsync();
 
-    const searchText = request.query.searchText;
+    let searchText = request.query.searchText;
     if (searchText) {
-        heroes = heroes.find(heroes => heroes.name.toLowerCase().startsWith(searchText.toLowerCase()));
+        searchText = searchText.toLowerCase();
+        heroes = heroes.find(heroes => heroes.name.toLowerCase().startsWith(searchText));
     }
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    response.end(JSON.stringify(heroes));
+    completeResponse(response, 200, heroes);
 });
 
 app.get('/:id', async(request, response) => {
     console.log('GET hero by id: ', request.params.id);
 
-    let heroes = await GetHeroes();
+    let heroes = await repository.getHeroesAsync();
 
     const id = parseInt(request.params.id);
     heroes = heroes.find(heroes => heroes.id == id);
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    response.end(JSON.stringify(heroes));
+    completeResponse(response, 200, heroes);
 });
 
 app.post('/', async(request, response) => {
-    console.log('Create hero');
-
     const hero = request.body;
-    await CreateHero(hero);
+    await repository.createHeroAsync(hero);
 
-    response.statusCode = 201;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    response.end(JSON.stringify(hero));
+    completeResponse(response, 200, hero);
 });
 
 app.delete('/:id', async(request, response) => {
     console.log('Delete hero by id: ', request.params.id);
 
     const id = parseInt(request.params.id);
-    await DeleteHero(id);
+    await repository.deleteHeroAsync(id);
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    response.end();
+    completeResponse(response, 200);
 });
 
 app.put('/', async(request, response) => {
-    console.log('Update hero');
     const hero = request.body;
 
-    await UpdateHero(hero);
+    await repository.updateHeroAsync(hero);
 
-    response.statusCode = 200;
+    completeResponse(response, 200);
+});
+
+const completeResponse = (response, statusCode, body) => {
+    response.statusCode = statusCode;
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
-    response.end();
-});
+    if (body) {
+        response.end(JSON.stringify(body));
+    } else {
+        response.end();
+    }
+}
 
 // setup listening
 const hostname = 'localhost';
